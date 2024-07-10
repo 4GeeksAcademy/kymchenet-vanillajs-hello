a90327e9cb4efa4267ae77e5f9d2bd7268ad56bd
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-07-10 17:56:27 +0000

    excuse js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo From the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["Un", "El"];
+  let subject = ["gato", "vecino", "niño", "taxista", "robot", "perro"];
+  let action = ["se llevó mi", "rompió mi", "se comió mi", "escondió mi"];
+  let posesion = ["bicicleta", "movil", "coche", "zapato"];
+  let where = ["en la calle", "en la cocina", "en el jardin", "en la escuela"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let posesionIndex = Math.floor(Math.random() * posesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    posesion[posesionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
